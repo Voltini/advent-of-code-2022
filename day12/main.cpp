569dad174b01c1c43d2680f1114f25ad4f1ca111
kssima <aymane.kssim@etu.inp-n7.fr>
2022-12-13 22:06:47 +0100

    day 12 end

--- day12/main.cpp
+++ day12/main.cpp
@@ -5,51 +5,68 @@
 #include <string>
 #include <set>
 #include <cstring>
+#include <queue>
+#include <utility>
 
-#define WIDTH 9
-#define HEIGHT 5
+#define WIDTH 162
+#define HEIGHT 41
+
+bool is_valid(int x, int y)
+{
+    return x - 1 >= 0 && x + 1 < HEIGHT && y - 1 >= 0 && y + 1 < HEIGHT;
+}
 
 int propagate(char map[HEIGHT][WIDTH], std::set<std::pair<int, int>> visited, int x, int y)
 {
-    if (map[x][y] == 'E')
+    std::priority_queue<std::pair<int, int>> current_nodes;
+    current_nodes.push(std::make_pair(x, y));
+    std::priority_queue<std::pair<int, int>> next_nodes;
+    int dist(0);
+    while (map[x][y] != 'E')
     {
-        return 0;
+        dist++;
+        while (!current_nodes.empty()) {
+            std::pair<int, int> point = current_nodes.top();
+            if(map[x][y] == 's')
+            {
+                int a = 0;
+            }
+            current_nodes.pop();
+            if (map[point.first][point.second] == 'E')
+            {
+                return dist;
+            }
+            if (point.first >= 1 &&
+                (map[point.first - 1][point.second] <= map[point.first][point.second] + 1 || (map[point.first][point.second] == 'z' && map[point.first - 1][point.second] == 'E')) &&
+                visited.insert(std::pair<int, int>(point.first - 1, point.second)).second)
+            {
+                next_nodes.push(std::make_pair(point.first - 1, point.second));
+            }
+            if (point.second >= 1 &&
+                (map[point.first][point.second - 1] <= map[point.first][point.second] + 1 || (map[point.first][point.second] == 'z' && map[point.first][point.second - 1] == 'E')) &&
+                visited.insert(std::pair<int, int>(point.first, point.second - 1)).second)
+            {
+                next_nodes.push(std::make_pair(point.first, point.second - 1));
+            }
+            if (point.first <= HEIGHT - 2 &&
+                (map[point.first + 1][point.second] <= map[point.first][point.second] + 1 || (map[point.first][point.second] == 'z' && map[point.first + 1][point.second] == 'E')) &&
+                visited.insert(std::pair<int, int>(point.first + 1, point.second)).second)
+            {
+                next_nodes.push(std::make_pair(point.first + 1, point.second));
+            }
+            if (point.second <= WIDTH - 2 &&
+                (map[point.first][point.second + 1] <= map[point.first][point.second] + 1 || (map[point.first][point.second] == 'z' && map[point.first][point.second + 1] == 'E')) &&
+                visited.insert(std::pair<int, int>(point.first, point.second + 1)).second)
+            {
+                next_nodes.push(std::make_pair(point.first, point.second + 1));
+            }
+            x = point.first;
+            y = point.second;
+        }
+        current_nodes = next_nodes;
+        next_nodes = std::priority_queue<std::pair<int, int>>();
     }
-    
-    int neighbours[4] = {HEIGHT * WIDTH + 1,
-    HEIGHT * WIDTH + 1,
-    HEIGHT * WIDTH + 1,
-    HEIGHT * WIDTH + 1};
-    if(x >= 1 && 
-    (map[x - 1][y] == map[x][y] || map[x - 1][y] == map[x][y] + 1 || map[x - 1][y] == map[x][y] + 1) &&
-    visited.insert(std::pair<int, int>(x - 1, y)).second)
-    {
-        neighbours[0] = 1 + propagate(map, visited, x - 1, y);
-    }
-    if(y >= 1 &&
-    (map[x][y - 1] == map[x][y] || map[x][y - 1] == map[x][y] + 1 || map[x][y - 1] == map[x][y] + 1) &&
-    visited.insert(std::pair<int, int>(x, y - 1)).second)
-    {
-        neighbours[1] = 1 + propagate(map, visited, x, y - 1);
-    }
-    if(x <= WIDTH - 2 &&
-    (map[x + 1][y] == map[x][y] || map[x + 1][y] == map[x][y] + 1 || map[x + 1][y] == map[x][y] + 1) &&
-    visited.insert(std::pair<int, int>(x + 1, y)).second)
-    {
-        neighbours[2] = 1 + propagate(map, visited, x + 1, y);
-    }
-    if (y <= HEIGHT - 2 &&
-    (map[x][y + 1] == map[x][y] || map[x][y + 1] == map[x][y] + 1 || map[x][y + 1] == map[x][y] + 1) &&
-    visited.insert(std::pair<int, int>(x, y + 1)).second)
-    {
-        neighbours[3] = 1 + propagate(map, visited, x, y + 1);
-    }
-    int min = neighbours[0];
-    for (size_t i = 1; i < 4; i++)
-    {
-        if(min > neighbours[i]) min = neighbours[i];
-    }
-    return min;
+    return 0;
 }
 
 int main() {
@@ -58,7 +75,7 @@ int main() {
     char map[HEIGHT][WIDTH];
     int i(0);
     std::pair<int, int> start;
-    for(std::string line; std::getline(file, line);)
+    for (std::string line; std::getline(file, line);)
     {
         strcpy(map[i], line.c_str());
         if (map[i][0] == 'S')
@@ -68,5 +85,11 @@ int main() {
         }
         i++;
     }
-    std::cout << propagate(map, visited, (*visited.begin()).first, (*visited.begin()).second) << std::endl;
+    std::set<int> dists;
+    for (size_t i = 0; i < HEIGHT; i++)
+    {
+        dists.insert(propagate(map, visited, i, 0));
+        visited.clear();
+    }
+    std::cout << *dists.begin() - 1 << std::endl;
 }
